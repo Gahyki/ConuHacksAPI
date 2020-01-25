fd180ed4eee59d862b4a8b947b444a3fd2442018
Gahyki <jason.k99@hotmail.com>
2020-01-25 16:36:30 -0500

    events id api

--- routes/events.js
+++ routes/events.js
@@ -9,14 +9,42 @@ router.get('/:id', async (req, res) => {
     if(utils.isEmptyOrNull(req.params, 'id'))
         return res.status(HTTP_BAD_REQUEST).json({ error: 'Invalid event id.' });
 
-    let { id } = req.params;
+    let { id } = req.params; //selected event id
 
     try {
-
-        let event = await db('events').select().where('id', id).first();
+        //comparing event ids
+        let event = await db('events').select().where('id', id).first(); 
 
         if(utils.isNullOrUndefined(event))
-            return res.json({ error: 'No such event.' })
+            return res.json({ error: 'No such event.' });
+
+        //Arrays to be read
+        event.jobs = JSON.parse(event.jobs);
+
+        //selecting queries
+        let jobsQuery = db('jobs').select();
+        let skillsQuery = db('skills').select();
+        let tasksQuery = db('tasks').select();
+
+        if(event.jobs.length > 0) {
+            event.jobs.forEach(jobID => jobsQuery.where('id', jobID));
+            event.jobs = await jobsQuery;
+        }
+
+        event.jobs.forEach(job => { 
+            job.tasks.forEach(taskID => tasksQuery.where('id', taskID));
+            job.skills.forEach(skillID => skillsQuery.where('id', skillID));
+        });
+
+        let tasks = await tasksQuery;
+        let skills = await skillsQuery;
+
+        event.jobs.forEach(job => {
+            for(let i in job.tasks)
+                job.tasks[i] = tasks.find(t => t.id === job.tasks[i]);
+            for(let i in job.skills)
+                job.skills[i] = skills.find(s => s.id === job.skills[i]);        
+        });
 
         res.json(event);
 
